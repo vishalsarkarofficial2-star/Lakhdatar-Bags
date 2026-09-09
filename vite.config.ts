6067d1be7f7a7c945a9b57595f29a18952904d93
vishalsarkarofficial2-star <vishalsarkarofficial2@gmail.com>
2026-09-09 12:22:01 +0530

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/YOUR_REPO_NAME/',
+    base: '/Lakhdatar-Bags/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
